0ab66855c269e10134f5aa59faa44dbe0c4de5a9
Zane Bliss <zanebliss@icloud.com>
2024-10-14 08:05:04 -0500

    add more `quiet` overrides

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,7 @@ if not vim.loop.fs_stat(lazypath) then
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    '--branch=stable',   -- latest stable release
     lazypath,
   })
 end
@@ -20,7 +20,19 @@ require('lazy').setup('plugins')
 
 require('config.mappings')
 
-vim.cmd 'colorscheme quiet'
-
-vim.cmd [[highlight! NormalFloat guibg=#2E2E2E]]
-vim.cmd [[highlight! Pmenu guifg=#FFFFFF guibg=#2E2E2E]]
+vim.cmd('color quiet')
+vim.cmd('set bg=dark')
+vim.cmd [[highlight! Normal guibg=none]]
+vim.cmd [[highlight! NormalFloat guibg=none]]
+vim.cmd [[highlight! Pmenu guifg=#FFFFFF guibg=none]]
+vim.cmd [[highlight! MatchParen guifg=#00afff]]
+vim.cmd [[highlight! Comment guifg=#707070 guibg=NONE gui=italic]]
+vim.cmd [[highlight! DiagnosticUnderlineHint gui=undercurl]]
+vim.cmd [[highlight! DiagnosticUnderlineError gui=undercurl guisp=Red]]
+vim.cmd [[highlight! DiagnosticUnderlineWarn  gui=undercurl guisp=Orange]]
+vim.cmd [[highlight! DiagnosticUnderlineInfo gui=undercurl guisp=LightBlue]]
+vim.cmd [[highlight! DiagnosticUnderlineHint gui=undercurl guisp=LightGrey]]
+vim.cmd [[highlight! DiagnosticUnderlineOk gui=undercurl guisp=LightGreen]]
+vim.cmd [[highlight! DiagnosticHint gui=italic guifg=#707070]]
+vim.cmd [[highlight! DiagnosticWarn gui=italic guifg=Orange]]
+vim.cmd [[highlight! DiagnosticError gui=italic guifg=Red]]
